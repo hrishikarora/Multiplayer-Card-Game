b6caf891db51b433cc76a6052e7efd93d5209094
Hrishik Arora <hrishik.arora@gmail.com>
2025-11-24 14:53:27 +0530

    Basic multiplayer setup

---
+++ Card Game/Assets/CardGame/Scripts/Core/GameConstants.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public static class GameConstants
+{
+    public const int MAX_PLAYERS = 2;
+}
---
+++ Card Game/Assets/CardGame/Scripts/Networking/PhotonManager.cs
@@ -0,0 +1,64 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+/// <summary>
+/// Handles Photon connection
+/// </summary>
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] string roomName = "CardGame";
+
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    void Start()
+    {
+        Connect();
+    }
+
+    public void Connect()
+    {
+        if (PhotonNetwork.IsConnected)
+            return;
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master");
+
+        PhotonNetwork.JoinOrCreateRoom(
+            roomName,
+            new RoomOptions { MaxPlayers = maxPlayers },
+            TypedLobby.Default
+        );
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log($"Joined room {PhotonNetwork.CurrentRoom.Name}: {PhotonNetwork.CurrentRoom.PlayerCount}/{PhotonNetwork.CurrentRoom.MaxPlayers}");
+
+        if (PhotonNetwork.IsMasterClient &&
+            PhotonNetwork.CurrentRoom.PlayerCount == maxPlayers)
+        {
+            //TODO: Load new scene
+        }
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.LogError($"Join room failed: {returnCode} - {message}");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarning($"Disconnected from Photon: {cause}");
+    }
+
+    private byte maxPlayers => GameConstants.MAX_PLAYERS;
+
+}
